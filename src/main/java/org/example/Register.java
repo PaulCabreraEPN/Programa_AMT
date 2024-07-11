599023cebaba71a7515b968b24867fd5b32bb460
1-21-01-E043-10\POO <paul.cabrera@epn.edu.ec>
2024-07-11 08:40:19 -0500

    Register completo

--- src/main/java/org/example/Register.java
+++ src/main/java/org/example/Register.java
@@ -1,16 +1,76 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Register {
     public JPanel MainPanel;
-    private JTextField textField1;
-    private JPasswordField passwordField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
+    private JTextField placa;
+    private JTextField modelo;
+    private JTextField combustible;
+    private JTextField color;
+    private JTextField propietario;
     private JButton registrarButton;
     private JButton cancelarButton;
     private JLabel Estado;
+    private JTextField cilindraje;
+    private JTextField marca;
+
+    String url="jdbc:mysql://localhost:3306/AMT";
+    String user="root";
+    String passw="123456";
+
+    vehiculos vehiculo = new vehiculos();
+
+    public Register() {
+        registrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                vehiculo.setPlaca(placa.getText());
+                vehiculo.setMarca(marca.getText());
+                vehiculo.setModelo(modelo.getText());
+                vehiculo.setClindraje(cilindraje.getText());
+                vehiculo.setTipo_combustible(combustible.getText());
+                vehiculo.setColor(color.getText());
+                vehiculo.setPropietario(propietario.getText());
+
+                try (Connection connection = DriverManager.getConnection(url,user,passw)) {
+                    System.out.println("Connected");
+                    String sql = "insert into vehiculo(Placa,Marca,Modelo,Cilindraje,Tipo_combustible,Color,Propietario) values (?,?,?,?,?,?,?)";
+                    PreparedStatement statement = connection.prepareStatement(sql);
+                    statement.setString(1, vehiculo.getPlaca());
+                    statement.setString(2, vehiculo.getMarca());
+                    statement.setString(3, vehiculo.getModelo());
+                    statement.setString(4, vehiculo.getClindraje());
+                    statement.setString(5, vehiculo.getTipo_combustible());
+                    statement.setString(6, vehiculo.getColor());
+                    statement.setString(7, vehiculo.getPropietario());
+
+                    statement.executeUpdate();
+                    Estado.setText("Registro Exitoso");
+
+                }catch (SQLException e1){
+                    System.out.println(e1.getMessage());
+                    Estado.setText("Algo Salió mal :(");
+                }
+            }
+        });
+        cancelarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                placa.setText(null);
+                marca.setText(null);
+                modelo.setText(null);
+                cilindraje.setText(null);
+                combustible.setText(null);
+                color.setText(null);
+                propietario.setText(null);
+            }
+        });
+    }
 }
--- src/main/java/org/example/vehiculos.java
+++ src/main/java/org/example/vehiculos.java
@@ -1,5 +1,95 @@
 package org.example;
 
 public class vehiculos {
+    private String Placa;
+    private String Marca;
+    private String Modelo;
+    private String Clindraje;
+    private String Tipo_combustible;
+    private String Color;
+    private String Propietario;
 
+    /*Consructores*/
+    public vehiculos() {
+    }
+
+    public vehiculos(String placa, String marca, String modelo, String clindraje, String tipo_combustible, String color, String propietario) {
+        Placa = placa;
+        Marca = marca;
+        Modelo = modelo;
+        Clindraje = clindraje;
+        Tipo_combustible = tipo_combustible;
+        Color = color;
+        Propietario = propietario;
+    }
+
+    /*getters & setters*/
+    public String getPlaca() {
+        return Placa;
+    }
+
+    public void setPlaca(String placa) {
+        Placa = placa;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String marca) {
+        Marca = marca;
+    }
+
+    public String getModelo() {
+        return Modelo;
+    }
+
+    public void setModelo(String modelo) {
+        Modelo = modelo;
+    }
+
+    public String getClindraje() {
+        return Clindraje;
+    }
+
+    public void setClindraje(String clindraje) {
+        Clindraje = clindraje;
+    }
+
+    public String getTipo_combustible() {
+        return Tipo_combustible;
+    }
+
+    public void setTipo_combustible(String tipo_combustible) {
+        Tipo_combustible = tipo_combustible;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public String getPropietario() {
+        return Propietario;
+    }
+
+    public void setPropietario(String propietario) {
+        Propietario = propietario;
+    }
+
+    /*Metodos*/
+    public void mostrar_vehiculo(){
+
+        System.out.println(">>>>>  Vehiculo Registrados  <<<<<");
+        System.out.println("Placa: " + this.getPlaca());
+        System.out.println("Marca: " + this.getMarca());
+        System.out.println("Modelo: " + this.getModelo());
+        System.out.println("Clindraje: " + this.getClindraje());
+        System.out.println("Tipo combustible: " + this.getTipo_combustible());
+        System.out.println("Color: " + this.getColor());
+        System.out.println("Propietario: " + this.getPropietario());
+    }
 }
